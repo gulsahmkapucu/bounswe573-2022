bfe7a8b0e1ff7d52df970a5dc2d0c91f5a023775
Gulsah <g.mervekapucu@hotmail.com>
2022-04-27 00:17:50 +0300

    Django Girls app, first commit

---
+++ mysite/connectmysql.py
@@ -0,0 +1,4 @@
+import mysql.connector
+# cnx = mysql.connector.connect(user='gulsah', password='password',
+#                               host='127.0.0.1',
+#                               database='employees')
